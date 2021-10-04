60ded527905d9a2417e7bad802971258c12fbc4f
BK Uddin <1bkhossain@gmail.com>
2021-10-04 19:27:52 +0600

    Banner Image Updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,44 +8,36 @@ const Header = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://images.pexels.com/photos/36704/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+            src="https://i.ibb.co/P6pTr6w/Group-1.png"
             alt="First slide"
           />
           <Carousel.Caption>
-            <h3> Quran Recitation tutors in Bangladesh</h3>
-            <p>
-              A professional Online Quran Teacher with tajweed Through Bengali
-              or English medium.
-            </p>
+            <h3></h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://images.pexels.com/photos/7249191/pexels-photo-7249191.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            src="https://i.ibb.co/Cbb38Lw/Group-2.png"
             alt="Second slide"
           />
 
           <Carousel.Caption>
-            <h3>Hire the best online Quran tutors for kids & adults</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            <h3></h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="https://images.pexels.com/photos/7249178/pexels-photo-7249178.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+            src="https://i.ibb.co/PcxfW2P/Group-3.png"
             alt="Third slide"
           />
 
           <Carousel.Caption>
-            <h3>
-              QURAN LEARN offers one of the most effective Arabic and online
-              Quran teaching websites
-            </h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
+            <h3></h3>
+            <p></p>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
